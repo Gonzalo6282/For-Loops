ff13a75e22e2875ad303a40f346a6deb669876a4
Gonzalo6282 <gonzalo6282@gmail.com>
2014-11-30 12:00:08 -0800

    For Loops

--- For Loops/LNViewController.m
+++ For Loops/LNViewController.m
@@ -18,6 +18,14 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    // for (int meditationHours = 1; meditationHours <= 100; meditationHours ++) {
+    //     NSLog(@"I am getting more enlightened");
+    // }
+    int strandOfWheat = 1;
+    for ( int i = 2; i <= 30; i ++){
+        strandOfWheat = strandOfWheat * 2;
+        NSLog(@"%i", strandOfWheat);
+    }
 }
 
 - (void)didReceiveMemoryWarning
